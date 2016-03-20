c7eb32cc12dc79bf9b6f78622a1bc8035c05393d
sesousy <sesousy@7gates.co>
2016-03-20 19:07:25 +0200

    [IMP] Payment & Journal migration

--- addons/account/migrations/9.0.1.1/post-migration.py
+++ addons/account/migrations/9.0.1.1/post-migration.py
@@ -14,17 +14,36 @@ MAP_INVOICE_TYPE_PARTNER_TYPE = {
     'in_refund': 'supplier',
 }
 
-PAYMENT_METHODS = {
-					'bank': {'inbound':1, 'outbound':1},
-					'cash': {'inbound':1, 'outbound':1},
-					'general': {'inbound':1, 'outbound':1},
-					'purchase': {'inbound':1, 'outbound':1},
-					'sale': {'inbound':1, 'outbound':1},
-				}
+def migrate_account_type(cr, registry):
+    logger.info('Starting migrate_account_type .....')
+    """."""
+    a_account_obj = registry['account.account']
 
-  
+    cr.execute(""" update account_account set type='receivable' where user_type_id=2 """)
+    cr.execute(""" update account_account set type='payable' where user_type_id=3 """)
+
+          
+def migrate_payment_method(cr, registry):
+    """."""
+    logger.info('Starting migrate_payment_method .....')
+    a_journal_obj = registry['account.journal']
+    #a_pay_meth_obj = registry['account.payment.method']
+    #inbound_payment_method_ids = a_pay_meth_obj.search(cr, uid, [('payment_type', '=', 'inbound')])
+    #outbound_payment_method_ids = a_pay_meth_obj.search(cr, uid, [('payment_type', '=', 'outbound')])
+    journal_ids = a_journal_obj.search(cr, uid, [('type', 'in', ('bank','cash'))])
+    #journals = a_journal_obj.browse(cr, uid, journal_ids)
+    for journal_id in journal_ids:
+        #ins = list(set(inbound_payment_method_ids) - set(journal_id.inbound_payment_method_ids.mapped('id')))
+        #outs = list(set(outbound_payment_method_ids) - set(journal_id.outbound_payment_method_ids.mapped('id')))
+        # logger.info('Journal: %s', journal_id)
+        a_journal_obj.write(cr, uid, [journal_id], {'inbound_payment_method_ids': (4, 1, None),
+                                                'outbound_payment_method_ids': (4, 2, None)})
+        a_journal_obj.write(cr, uid, [journal_id], {'inbound_payment_method_ids': (4, 3, None),
+                                                'outbound_payment_method_ids': (4, 4, None)})
+        
 def migrate_move_invoice(cr, registry):
     """."""
+    logger.info('Starting migrate_move_invoice .....')
     a_move_line_obj = registry['account.move.line']
 
     cr.execute('SELECT l.id, i.id ' \
@@ -33,65 +52,115 @@ def migrate_move_invoice(cr, registry):
 
     for line_id, invoice_id in cr.fetchall():
         a_move_line_obj.write(cr, uid, [line_id], {'invoice_id': invoice_id})
-          
-def migrate_payment_method(cr, registry):
-    """Create a Push rule for each pair of locations linked. Will break if
-    there are multiple warehouses for the same company."""
-    a_pay_meth_obj = registry['account.payment.method']
-    PAYMENT_METHODS['bank']['inbound'] = a_pay_meth_obj.create(cr, uid, {'name':'Bank', 'code':'bank', 'payment_type':'inbound'})
-    PAYMENT_METHODS['bank']['outbound'] = a_pay_meth_obj.create(cr, uid, {'name':'Bank', 'code':'bank', 'payment_type':'outbound'})
-    PAYMENT_METHODS['cash']['inbound'] = a_pay_meth_obj.create(cr, uid, {'name':'Cash', 'code':'cash', 'payment_type':'inbound'})
-    PAYMENT_METHODS['cash']['outbound'] = a_pay_meth_obj.create(cr, uid, {'name':'Cash', 'code':'cash', 'payment_type':'outbound'})
-    PAYMENT_METHODS['general']['inbound'] = a_pay_meth_obj.create(cr, uid, {'name':'General', 'code':'general', 'payment_type':'inbound'})
-    PAYMENT_METHODS['general']['outbound'] = a_pay_meth_obj.create(cr, uid, {'name':'General', 'code':'general', 'payment_type':'outbound'})
-    PAYMENT_METHODS['purchase']['inbound'] = a_pay_meth_obj.create(cr, uid, {'name':'Purchase', 'code':'purchase', 'payment_type':'inbound'})
-    PAYMENT_METHODS['purchase']['outbound'] = a_pay_meth_obj.create(cr, uid, {'name':'Purchase', 'code':'purchase', 'payment_type':'outbound'})
-    PAYMENT_METHODS['sale']['inbound'] = a_pay_meth_obj.create(cr, uid, {'name':'Sale', 'code':'sale', 'payment_type':'inbound'})
-    PAYMENT_METHODS['sale']['outbound'] = a_pay_meth_obj.create(cr, uid, {'name':'Sale', 'code':'sale', 'payment_type':'outbound'})
- 
-
-def migrate_payment_info_JSON(cr, registry):
-    a_invoice_obj = registry['account.invoice']
-    
-    cr.execute('SELECT i.id  FROM account_invoice i')
-
-    for invoice_id in cr.fetchall():
-        invoice = a_invoice_obj.browse(cr, uid, invoice_id)
-        invoice.payments_widget = json.dumps(False)
-        if invoice.payment_move_line_ids:
-            info = {'title': _('Less Payment'), 'outstanding': False, 'content': []}
-            for payment in invoice.payment_move_line_ids:
-                #we don't take into account the movement created due to a change difference
-                if payment.currency_id and payment.move_id.rate_diff_partial_rec_id:
-                    continue
-                if invoice.type in ('out_invoice', 'in_refund'):
-                    amount = sum([p.amount for p in payment.matched_debit_ids if p.debit_move_id in self.move_id.line_ids])
-                    amount_currency = sum([p.amount_currency for p in payment.matched_debit_ids if p.debit_move_id in self.move_id.line_ids])
-                elif invoice.type in ('in_invoice', 'out_refund'):
-                    amount = sum([p.amount for p in payment.matched_credit_ids if p.credit_move_id in self.move_id.line_ids])
-                    amount_currency = sum([p.amount_currency for p in payment.matched_credit_ids if p.credit_move_id in self.move_id.line_ids])
-                # get the payment value in invoice currency
-                if payment.currency_id and amount_currency != 0:
-                    currency_id = payment.currency_id
-                    amount_to_show = -amount_currency
-                else:
-                    currency_id = payment.company_id.currency_id
-                    amount_to_show = -amount
-                info['content'].append({
-                    'name': payment.name,
-                    'journal_name': payment.journal_id.name,
-                    'amount': amount_to_show,
-                    'currency': currency_id.symbol,
-                    'digits': [69, currency_id.decimal_places],
-                    'position': currency_id.position,
-                    'date': payment.date,
-                    'payment_id': payment.id,
-                    'move_id': payment.move_id.id,
-                    'ref': payment.move_id.ref,
-                })
-        	a_invoice_obj.write(cr, uid, [invoice_id], {'payments_widget': json.dumps(info)})
 
 def migrate_payment(cr, registry):
+    """Create a Push rule for each pair of locations linked. Will break if
+    there are multiple warehouses for the same company."""
+    logger.info('Starting migrate_payment .....')
+    a_move_line_obj = registry['account.move.line']
+    a_payment_obj = registry['account.payment']
+    a_journal_obj = registry['account.journal']
+    ir_sequence_obj = registry['ir.sequence']
+        
+
+    cr.execute(""" select v.id, v.journal_id, v.type, v.date, v.reference, v.amount, v.payment_rate_currency_id,
+                        v.company_id, v.partner_id, v.move_id from account_voucher v join account_journal j on(v.journal_id=j.id) 
+                            where v.type in ('receipt','payment') and j.type in ('bank', 'cash') and v.amount > 0""" )
+    voucher_ids = cr.fetchall()
+    
+
+    for voucher in voucher_ids:
+        logger.info("Treating Voucher: %s ", voucher)
+        # if float(voucher[5]) <= 0.0:
+        #    continue
+        journal = a_journal_obj.browse(cr, uid, voucher[1])
+        
+        payment_type = voucher[2] == 'receipt' and 'inbound' or 'outbound'
+        partner_type = voucher[2] == 'receipt' and 'customer' or 'supplier'
+        #journal_payment_methods = payment_type == 'inbound' and journal.inbound_payment_method_ids or journal.outbound_payment_method_ids
+        #payment_method_id = journal_payment_methods[0].id
+        payment_method_id = payment_type == 'inbound' and 1 or 2
+        if partner_type == 'customer':
+            if payment_type == 'inbound':
+                sequence = 'account.sequence_payment_customer_invoice'
+            if payment_type == 'outbound':
+                sequence = 'account.sequence_payment_customer_refund'
+        if partner_type == 'supplier':
+            if payment_type == 'inbound':
+                sequence = 'account.sequence_payment_supplier_refund'
+            if payment_type == 'outbound':
+                sequence = 'account.sequence_payment_supplier_invoice'
+                
+        seq_id = registry['ir.model.data'].xmlid_to_res_id(cr, uid, sequence)
+        #sequence_id = ir_sequence_obj.browse(cr, uid, seq_id)
+        #context['fiscalyear_id'] = period.fiscalyear_id.id
+        #journal = self.pool.get('account.journal').browse(cr, uid, journal_id, None)
+        #return obj_seq.next_by_id(cr, uid, journal.sequence_id.id, context=context)
+
+        
+        # context['ir_sequence_date'] = voucher[3]    
+        payment_name = ir_sequence_obj.next_by_id(cr, uid, seq_id, context={'ir_sequence_date':voucher[3]})
+        
+        cr.execute(""" select id, move_line_id, type  from account_voucher_line where voucher_id = %s """ % voucher[0])
+        voucher_line_ids = cr.fetchall()
+        
+        cr.execute(""" SELECT i.id FROM account_invoice i join account_move_line ml on (i.move_id = ml.move_id) 
+                            WHERE ml.id in %s""", (tuple(x[1] for x in voucher_line_ids), ))
+        
+        invoice_ids = [x[0] for x in cr.fetchall()]
+        
+        vals = {
+            'name': payment_name,
+            'journal_id': journal.id,
+            'payment_method_id': payment_method_id,
+            'payment_date': voucher[3],
+            'communication': voucher[4],
+            'invoice_ids': (6, None, invoice_ids),  # [(4, invoice_id, None)],
+            'payment_type': payment_type,
+            'amount': voucher[5],
+            'currency_id': voucher[6],
+            'partner_id': voucher[8],
+            'partner_type': partner_type,
+            'company_id': voucher[7],
+        }
+        logger.info("Creating a payment: %s ", vals)
+        a_payment_id = a_payment_obj.create(cr, uid, vals)
+        
+        if voucher[2] == 'receipt': 
+            cr.execute(""" (select id from account_move_line 
+                            where  move_id=%s and reconcile_ref is not null) 
+                            union 
+                            (select id from account_move_line 
+                            where  move_id=%s and debit > 0  and reconcile_ref is null) """,
+                                (voucher[9], voucher[9], ))
+        else:
+            cr.execute(""" (select id from account_move_line 
+                            where  move_id=%s and reconcile_ref is not null) 
+                            union 
+                            (select id from account_move_line 
+                            where  move_id=%s and credit > 0  and reconcile_ref is null) """,
+                                (voucher[9], voucher[9], ))
+                        
+        payment_move_lines = [x[0] for x in cr.fetchall()]
+        logger.info("Payment Journal Items %s: %s ", a_payment_id, payment_move_lines)
+        a_move_line_obj.write(cr, uid, payment_move_lines, {'payment_id': a_payment_id})
+        
+        cr.execute(""" select distinct reconcile_ref from account_move_line
+                                            where move_id = %s and reconcile_ref is not null""", 
+                                            (voucher[9], ))
+        reconcile_refs = [x[0] for x in cr.fetchall()]
+        
+        for reconcile_ref in reconcile_refs:
+            cr.execute(""" select id from account_move_line 
+                            where  reconcile_ref = %s """, (reconcile_ref, ))
+            reconcile_move_ids = [x[0] for x in cr.fetchall()]
+            a_reconcile_lines = a_move_line_obj.browse(cr, uid, reconcile_move_ids)
+            result_reconcile_lines = a_reconcile_lines.auto_reconcile_lines()
+            logger.info("result_reconcile_lines %s: %s ", reconcile_ref, result_reconcile_lines.mapped('id'))
+        
+        
+
+def migrate_payment_invoice(cr, registry):
     """Create a Push rule for each pair of locations linked. Will break if
     there are multiple warehouses for the same company."""
     a_move_obj = registry['account.move']
@@ -128,13 +197,15 @@ def migrate_payment(cr, registry):
         if not lines:
         	continue
 
-        #journal = a_journal_obj.browse(cr, uid, lines[0][1])
+        journal = a_journal_obj.browse(cr, uid, lines[0][1])
         
         payment_type = invoice.type in ('out_invoice', 'in_refund') and 'inbound' or 'outbound'
         payment_method_id = PAYMENT_METHODS[lines[0][2]][payment_type]
+        journal_payment_methods = payment_type == 'inbound' and journal.inbound_payment_method_ids or journal.outbound_payment_method_ids
+        payment_method_id = journal_payment_methods[0].id
         vals = {
 			'name': 'Draft Payment', # TODO: name by sequence
-            'journal_id': lines[0][1],
+            'journal_id': journal.id,
             'payment_method_id': payment_method_id,
             'payment_date': move.date,
             'communication': invoice.reference,
@@ -149,15 +220,16 @@ def migrate_payment(cr, registry):
         logger.info("Creating a payment: %s ", vals)
         a_payment_id = a_payment_obj.create(cr, uid, vals)
         for line in lines:
-         	a_move_line_obj.write(cr, uid, [line[0]], {'payment_id': a_payment_id})
+            a_move_line_obj.write(cr, uid, [line[0]], {'payment_id': a_payment_id})
+            cr.execute("""INSERT INTO account_invoice_account_move_line_rel (%s, %s) """ % (invoice.id, line[0])) 
         
 @openupgrade.migrate()
 def migrate(cr, version):
-	registry = RegistryManager.get(cr.dbname)
-	migrate_move_invoice(cr, registry)
-	migrate_payment_method(cr, registry)
-	migrate_payment(cr, registry)
-	migrate_payment_info_JSON(cr, registry)
+    registry = RegistryManager.get(cr.dbname)
+    migrate_account_type(cr, registry)
+    migrate_move_invoice(cr, registry)
+    migrate_payment_method(cr, registry)
+    migrate_payment(cr, registry)
 	
-   	cr.execute("UPDATE wkf_instance SET state='active' where state='deactive'")
+    cr.execute("UPDATE wkf_instance SET state='active' where state='deactive'")
     
--- addons/account/migrations/9.0.1.1/pre-migration.py
+++ addons/account/migrations/9.0.1.1/pre-migration.py
@@ -1,6 +1,124 @@
 from openupgradelib import openupgrade
 
-	
+column_renames = {
+    'account_account': [
+        ('active', None),
+        ('currency_mode', None),
+        ('parent_id', None),
+        ('parent_left', None),
+        ('parent_right', None),
+        ('shortcut', None),
+        ('type', None)
+    ],
+    'account_account_template': [
+        ('parent_id', None),
+        ('shortcut', None),
+        ('type', None),
+    ],
+    'account_account_type': [
+        ('close_method', None),
+        ('code', None),
+    ],
+    'account_analytic_line': [
+        ('journal_id', None),
+    ],
+    'account_bank_statement': [
+        ('closing_date', None),
+        ('period_id', None),
+    ],
+    'account_bank_statement_line': [
+        ('journal_entry_id', None),
+    ],
+    'account_cashbox_line': [
+        ('bank_statement_id', None),
+        ('number_closing', None),
+        ('number_opening', None),
+        ('pieces', None),
+    ],
+    'account_chart_template': [
+        ('account_root_id', None),
+        ('bank_account_view_id', None),
+        ('property_account_expense_opening', None),
+        ('property_account_income_opening', None),
+        ('tax_code_root_id', None),
+    ],
+    'account_invoice': [
+        ('check_total', None),
+        ('internal_number', None),
+        ('period_id', None),
+        ('supplier_invoice_number', None),
+    ],
+    'account_invoice_tax': [
+        ('base', None),
+        ('base_amount', None),
+        ('base_code_id', None),
+        ('tax_amount', None),
+        ('tax_code_id', None),
+    ],
+    'account_journal': [
+        ('allow_date', None),
+        ('analytic_journal_id', None),
+        ('cash_control', None),
+        ('centralisation', None),
+        ('entry_posted', None),
+        ('internal_account_id', None),
+        ('user_id', None),
+        ('with_last_closing_balance', None),
+    ],
+    'account_move': [
+        ('balance', None),
+        ('line_id', None),
+        ('period_id', None),
+        ('to_check', None),
+    ],
+    'account_move_line': [
+        ('account_tax_id', None),
+        ('centralisation', None),
+        ('date_created', None),
+        ('reconcile_id', None),
+        ('reconcile_partial_id', None),
+        ('state', None),
+        ('tax_amount', None),
+        ('tax_code_id', None),
+    ],
+    'account_tax': [
+        ('account_analytic_collected_id', None),
+        ('account_analytic_paid_id', None),
+        ('applicable_type', None),
+        ('base_code_id', None),
+        ('base_sign', None),
+        ('child_depend', None),
+        ('domain', None),
+        ('parent_id', None),
+        ('python_compute_inv', None),
+        ('ref_base_code_id', None),
+        ('ref_base_sign', None),
+        ('ref_tax_code_id', None),
+        ('ref_tax_sign', None),
+        ('tax_code_id', None),
+        ('tax_sign', None),
+    ],
+    'account_tax_template': [
+        ('applicable_type', None),
+        ('base_code_id', None),
+        ('base_sign', None),
+        ('child_depend', None),
+        ('domain', None),
+        ('parent_id', None),
+        ('python_compute_inv', None),
+        ('ref_base_code_id', None),
+        ('ref_base_sign', None),
+        ('ref_tax_code_id', None),
+        ('ref_tax_sign', None),
+        ('tax_code_id', None),
+        ('tax_sign', None),
+        ('type', None),
+    ],
+    'res_partner': [
+        ('vat_subjected', None),
+    ],
+}
+
 @openupgrade.migrate()
 def migrate(cr, version):
     cr.execute("UPDATE wkf_instance SET state='deactive' where state='active'")
